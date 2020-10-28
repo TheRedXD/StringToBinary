72567088f9c3196405c4c55274737db0f3c196c5
TheRedX <56270596+TheRedXD@users.noreply.github.com>
2020-10-28 16:16:48 +0200

    Added hex functionality and descriptions

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-function toBinary(i){if(typeof i!='string')throw new Error("Invalid");n=[];i.split("").forEach(m=>{b=(m.charCodeAt()).toString(2);while(!(b.length==8)){b=0+b}n.push(b)});n=n.join(" ");return n};module.exports=toBinary;
+/**
+ * @param {*} i Converts an ASCII string to binary. Usage: `stringToBinary("Your String Here")`
+ */
+function stringToBinary(i) {
+  if (typeof i != 'string') throw new Error("Invalid");
+  n = [];
+  i.split("").forEach(m => {
+    b = (m.charCodeAt()).toString(2);
+    while (!(b.length == 8)) {
+      b = 0 + b;
+    }
+    n.push(b); //I am in a node module yay
+  });
+  n = n.join(" ");
+  return n;
+};
+/**
+ * @param {*} i Converts an ASCII string to hex. Usage: `stringToHex("Your String Here")`
+ */
+function stringToHex(i) {
+  if (typeof i != 'string') throw new Error("Invalid");
+  n = [];
+  i.split("").forEach(m => {
+    b = (m.charCodeAt()).toString(16);
+    n.push(b);
+  });
+  n = n.join(" ");
+  return n;
+};
+module.exports = { stringToBinary, stringToHex };
