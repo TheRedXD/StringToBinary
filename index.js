cccfaa05953cdb94fe67ff0623d2886dfdcaf4c7
BlueBurgersTDD <58081413+BlueBurgersTDD@users.noreply.github.com>
2020-11-08 14:58:58 +0100

    Revert "Made better"
    This reverts commit 7b4cf55c33dd1542e8117deeb9b1779e13e5f0df.

--- index.js
+++ index.js
@@ -1,4 +1,59 @@
-var convertions = require('./conversions');
+var convertions = [{ //Made by http://github.com/BlueBurgersTDD
+    "aliases": ["binary", "bin", "b"],
+    "scripts": {
+      "translateTo": function (a) {
+        return a.split("").map(b => b.charCodeAt().toString(2)).map(b => {
+          while (b.length < 8) {
+            b = "0" + b
+          }
+          return b;
+        }).join(" ");
+      },
+      "translateFrom": function (a) {
+        return a.split(" ").map(b => String.fromCharCode(parseInt(b, 2))).join("");
+      }
+    }
+  },
+  {
+    "aliases": ["hex", "x"],
+    "scripts": {
+      "translateTo": function (a) {
+        return a.split("").map(b => b.charCodeAt().toString(16).toUpperCase()).join(" ");
+      },
+      "translateFrom": function (a) {
+        return a.split(" ").map(b => String.fromCharCode(parseInt(b, 16))).join("");
+      }
+    }
+  },
+  {
+    "aliases": ["base64", "b64", "base 64"],
+    "scripts": {
+      "translateTo": function (a) {
+        return Buffer.from(a).toString("base64");
+      },
+      "translateFrom": function (a) {
+        return Buffer.from(a, "base64").toString("utf-8");
+      }
+    }
+  },
+  {
+    "aliases": ["ternary", "3", "t"],
+    "scripts": {
+      "translateTo": function(a) {
+        return a.split("").map(b => b.charCodeAt().toString(3)).map(b => {
+          while (b.length < 6) {
+            b = "0" + b
+          }
+          return b;
+        }).join(" ");
+      },
+      "translateFrom": function (a) {
+        return a.split(" ").map(b => String.fromCharCode(parseInt(b, 3))).join("");
+      }
+    }
+  }
+];
+
 function translateTo(format, text) {
   format = convertions.find(a => a.aliases.some(b => b == format));
   if (!format) throw new Error("Invalid format!");
@@ -23,6 +78,3 @@ module.exports = {
   translateFrom,
   translate
 };
-convertions.forEach(convertion => {
-  module.exports[convertion.aliases[0]] = convertion.scripts;
-})
